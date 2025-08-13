fb4d501b68dec355621dfcce513297d024ae0650
Luan Tenorio <lluantenorio7@gmail.com>
2025-08-13 20:50:19 -0300

    set swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from './config/config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 function setValidation(app: INestApplication) {
   app.useGlobalPipes(new ValidationPipe({
@@ -11,11 +12,25 @@ function setValidation(app: INestApplication) {
   }));
 }
 
+function setSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Learn')
+    .setDescription('Learn endpoint Documentation')
+    .setVersion('1.0')
+    .addTag('learn')
+    .addBearerAuth({ type: "apiKey" })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = app.get(ConfigService).config.server.port;
 
   setValidation(app);
+  setSwagger(app);
   app.setGlobalPrefix("api");
   Logger.log(`Listening on port ${port}`, 'Bootstrap');
 
