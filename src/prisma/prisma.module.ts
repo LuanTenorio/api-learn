da40163c10ee9190d3dadfcb75b7136012df346e
Luan Tenorio <lluantenorio7@gmail.com>
2025-08-13 21:14:14 -0300

    Adicionando logs das querys

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,10 +1,16 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
+import { ConfigModule } from "src/config/config.module";
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+    imports: [
+        ConfigModule
+    ],
+    providers: [
+        PrismaService
+    ],
+    exports: [
+        PrismaService
+    ]
 })
-export class PrismaModule {
-
-}
+export class PrismaModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,41 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { INestApplication, Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '../../generated/prisma/client';
+import { ConfigService } from 'src/config/config.service';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
 
-    async onModuleInit() {
-        await this.$connect();
-    }
+  private logger = new Logger(PrismaService.name)
 
+  @Inject()
+  private configService: ConfigService
+
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' }
+      ],
+      errorFormat: 'colorless',
+    });
+  }
+
+  async onModuleInit() {
+    this.logs()
+    await this.$connect();
+  }
+
+  async logs() {
+    if (!this.configService.config.log.showQuery)
+      return
+
+    this.$on("query" as never, async ({ query, params, duration }: Prisma.QueryEvent) => {
+      this.logger.log(`[Query] ${duration}ms ${params} -- '${query}'`)
+    })
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit' as never, async () => {
+      await app.close();
+    });
+  }
 }
